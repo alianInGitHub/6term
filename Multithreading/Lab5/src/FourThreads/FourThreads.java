442be79e235efd4fdfa2b532140009e46e2bdcc8
Anastasiia Bondarenko <anastasia@localhost.localdomain>
2017-03-26 13:56:22 +0300

    class FourThreads was created

---
+++ Multithreading/Lab5/src/FourThreads/FourThreads.java
@@ -0,0 +1,16 @@
+package FourThreads;
+
+/**
+ * Created by anastasia on 3/26/17.
+ */
+public class FourThreads {
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        FourThreads task = new FourThreads();
+        task.run();
+    }
+}
