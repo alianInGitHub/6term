0845f2b9fb16c662f8ab4c6378185cf64f6d76c8
Anastasiia Bondarenko <anastasiabondarenko007@gmail.com>
2017-03-28 01:46:13 +0300

    Basic GUI was added

--- Multithreading/Lab6/src/Form.java
+++ Multithreading/Lab6/src/Form.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Created by anastasia on 3/28/17.
@@ -7,15 +8,58 @@ import java.awt.*;
 public class Form {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Game of life");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLayout(new BorderLayout());
+        Container content = frame.getContentPane();
+        content.setLayout(new BorderLayout());
+
         GameOfLife game = new GameOfLife();
-        frame.add(game, BorderLayout.CENTER);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
+        content.add(game, BorderLayout.CENTER);
+        content.add(createControlPanel(game), BorderLayout.NORTH);
+
+        frame.setSize(GameOfLife.WIDTH - 5, GameOfLife.HEIGHT + 70);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.addMouseListener(game);
-        frame.addKeyListener(game);
+    }
+
+    private static JPanel createControlPanel(GameOfLife gameOfLife) {
+        JPanel controls = new JPanel();
+        controls.setLayout(new BoxLayout(controls, BoxLayout.X_AXIS));
+
+        JButton startButton = new JButton("Start");
+        JButton pauseButton = new JButton("Pause");
+        JButton stopButton = new JButton("Stop");
+
+        ActionListener listener = e -> {
+            if (e.getSource() == startButton) {
+                if(startButton.getName() == "Start") {
+                    gameOfLife.run();
+                } else if(startButton.getName() == "Continue") {
+                    gameOfLife.notify();
+                }
+            }
+            if (e.getSource() == pauseButton) {
+                gameOfLife.pause();
+                startButton.setName("Continue");
+            }
+
+            if(e.getSource() == stopButton) {
+                gameOfLife.notify();
+                gameOfLife.stop();
+            }
+        };
+
+        startButton.addActionListener(listener);
+        pauseButton.addActionListener(listener);
+        stopButton.addActionListener(listener);
+
+        startButton.setBackground(Color.GREEN);
+        pauseButton.setBackground(Color.ORANGE);
+        stopButton.setBackground(Color.RED);
+
+        controls.add(startButton);
+        controls.add(pauseButton);
+        controls.add(stopButton);
+
+        return controls;
     }
 }
--- Multithreading/Lab6/src/GameOfLife.java
+++ Multithreading/Lab6/src/GameOfLife.java
@@ -1,7 +1,100 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 /**
  * Created by anastasia on 3/28/17.
  */
-public class GameOfLife extends JComponent {
+public class GameOfLife extends JComponent implements Runnable {
+    private ArrayList<ArrayList<Boolean>> grid;
+    private Graphics2D graphics2D;
+    private Image image;
+
+    private static final Color backgroundColor = Color.DARK_GRAY;
+    private static final int CELL_SIZE = 12;
+    public static final int HEIGHT = 720;
+    public static final int WIDTH = 960;
+
+    public GameOfLife() {
+        setSize(WIDTH, HEIGHT);
+        initialize();
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                selectCell(e.getPoint());
+            }
+        });
+    }
+
+    public void selectCell(Point locationOnCanvas) {
+
+    }
+
+    private void drawGrid(){
+
+        clear();
+        graphics2D.setColor(Color.GRAY);
+
+        for (int i = CELL_SIZE; i < WIDTH; i += CELL_SIZE)
+            graphics2D.drawLine(i, 0, i, HEIGHT);
+        for (int i = CELL_SIZE; i < HEIGHT; i += CELL_SIZE)
+            graphics2D.drawLine(0, i, WIDTH, i);
+
+        graphics2D.setColor(Color.GREEN);
+        for (int i = 0; i < WIDTH / CELL_SIZE; i ++) {
+            for (int j = 0; j < HEIGHT / CELL_SIZE; j++) {
+                if (grid.get(i).get(j))
+                    graphics2D.fillRect(i * CELL_SIZE + 1, j * CELL_SIZE + 1, 9, 9);
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        if(image == null) {
+            image = createImage(getSize().width, getSize().height);
+            graphics2D = (Graphics2D) image.getGraphics();
+            graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            clear();
+        }
+        drawGrid();
+        g.drawImage(image, 0, 0, null);
+
+    }
+
+    private void initialize() {
+        grid = new ArrayList<>();
+        for (int i = 0; i < 80; i++){
+            grid.add(new ArrayList<>());
+            for (int j = 0; j < 60; j++) {
+                grid.get(i).add(false);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void clear() {
+        graphics2D.setPaint(backgroundColor);
+        graphics2D.fillRect(0, 0, getWidth(), getHeight());
+        repaint();
+    }
+
+    public void pause() {
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        // interrupt threads
+    }
 }
