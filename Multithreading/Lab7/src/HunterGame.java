7bc27f312f1361ac2b2e055385c97be1b1116e96
Anastasiia Bondarenko <anastasiabondarenko007@gmail.com>
2017-04-05 10:57:46 +0300

    Fixed

--- Multithreading/Lab7/src/HunterGame.java
+++ Multithreading/Lab7/src/HunterGame.java
@@ -1,8 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
